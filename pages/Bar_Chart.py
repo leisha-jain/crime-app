c2d1a5b29cf1d416f61e60b3bae76c9e276a2f36
leisha-jain <leishajain20@gmail.com>
2025-07-05 13:39:44 +0530

    Update and rename Bar Chart.py to Bar_Chart.py

--- pages/Bar_Chart.py
+++ pages/Bar_Chart.py
@@ -2,12 +2,63 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")   
+st.markdown("""
+    <style>
+        
+        .topnav {
+            background-color: #333;
+            overflow: hidden;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            height: 50px;
+            z-index: 999999;
+        }
+
+        .topnav a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 26px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        /* Push content below navbar */
+        .main .block-container {
+            padding-top: 80px; /* increased from 60 to 80 for more spacing */
+        }
+            
+        
+    </style>
+
+    <div class="topnav">
+      <a href="/" target="_self">Home</a>
+      <a class="active" href="/Bar_Chart" target="_self">Bar Chart</a>
+      <a href="/CorrelationAllIndia" target="_self">Correlation (All India)</a>
+      <a href="/CorrelationStateWise" target="_self">Correlation (State Wise)</a>
+      <a href="/Heatmap" target="_self">Heatmap</a>
+      <a href="/Prediction" target="_self">Prediction</a>
+    </div>
+""", unsafe_allow_html=True) 
 st.title("Top 5 States by Crime")
 
 # Mapping of crime names to filenames
 crime_files = {
-    "Rape": "Rape.csv",
-    "Murder": "Murder.csv",
+    "Rape": "rape.csv",
+    "Murder": "murder.csv",
     "Kidnapping": "kidnapping.csv",
     "Crime Against Children": "crimeAgainstChildren.csv",
     "Dowry Death": "dowryDeaths.csv",
